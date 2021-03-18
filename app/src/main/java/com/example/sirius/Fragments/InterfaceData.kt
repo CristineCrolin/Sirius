185fc8bcfac7a64aff57054232c845fad34252e9
CristineCrolin <crolincristine@gmail.com>
2021-03-18 18:32:55 +0700

    Fragments

---
+++ app/src/main/java/com/example/sirius/Fragments/InterfaceData.kt
@@ -0,0 +1,4 @@
+package com.example.sirius.Fragments
+
+interface InterfaceData {
+}
---
+++ app/src/main/java/com/example/sirius/ktkReviews.kt
@@ -0,0 +1,11 @@
+package com.example.sirius
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ktkReviews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ktk_reviews)
+    }
+}
